739c6929b700b12245fae4ca151cccd58f40ea3f
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-12-05 20:05:57 +0700

    feat: button

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,11 +1,84 @@
-import { cva } from 'class-variance-authority'
+import { VariantProps, cva } from 'class-variance-authority'
+import { MouseEvent, PropsWithChildren } from 'react'
 
-const styles = cva([], {
-  variants: {},
+const styles = cva(['rounded-md', 'duration-100', 'focus:scale-95'], {
+  variants: {
+    intent: {
+      primary: ['bg-sky-500', 'hover:bg-sky-400', 'text-white'],
+      outline: [
+        'ring-1',
+        'text-sky-500',
+        'ring-sky-500',
+        'hover:border-sky-400',
+        'hover:text-sky-400',
+      ],
+      danger: ['bg-rose-500', 'hover:bg-rose-400', 'text-white'],
+      ghost: ['text-sky-500', 'hover:text-sky-400', 'hover:bg-slate-100'],
+    },
+    disabled: {
+      true: ['text-white', 'bg-slate-500', 'hover:bg-slate-500'],
+    },
+    disabledGhost: {
+      true: ['text-slate-500', 'bg-transparent', 'hover:bg-transparent'],
+    },
+    loading: {
+      true: [],
+    },
+    size: {
+      normal: ['px-3', 'py-2'],
+      compact: ['px-3', 'py-1'],
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'normal',
+  },
 })
 
-type Props = {}
+type Props = {
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void | Promise<void>
+  className?: string
+  icon?: `fa fa-${string}`
+} & Omit<VariantProps<typeof styles>, 'disabledGhost'>
 
-export const Button = (props: Props) => {
-  return <button>Boop</button>
+export const Button = ({
+  intent,
+  children,
+  disabled,
+  className,
+  size,
+  icon,
+  loading,
+  onClick,
+}: PropsWithChildren<Props>) => {
+  const isDisabled = disabled || loading
+  const disabledGhost = isDisabled && intent === 'ghost'
+
+  return (
+    <button
+      className={styles({
+        intent: isDisabled ? undefined : intent,
+        disabled: disabledGhost ? undefined : isDisabled,
+        className,
+        size,
+        disabledGhost,
+      })}
+      disabled={isDisabled || undefined}
+      onClick={onClick}
+    >
+      {icon && !loading ? (
+        <span className={`${icon} mr-2 translate-y-[0.5px]`} />
+      ) : null}
+
+      {loading ? (
+        <span
+          // @ts-ignore
+          style={{ '--fa-animation-timing': 'ease-in-out' }}
+          className="fa-solid fa-circle-notch fa-spin mr-2 relative top-[0.5px]"
+        />
+      ) : null}
+
+      {children}
+    </button>
+  )
 }
--- packages/ui/stories/Button.stories.tsx
+++ packages/ui/stories/Button.stories.tsx
@@ -5,9 +5,30 @@ import { Button } from '../components/Button'
 
 export default {
   component: Button,
+  argTypes: {
+    intent: {
+      control: {
+        type: 'select',
+        options: ['primary', 'outline', 'danger', 'ghost'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['normal', 'compact'],
+      },
+    },
+  },
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  intent: 'primary',
+  disabled: false,
+  loading: false,
+  icon: 'fa fa-home',
+  size: 'normal',
+  children: 'Boop',
+}
